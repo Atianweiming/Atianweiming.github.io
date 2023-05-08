25437a080aea205940e1564389d601afbe050b77
田伟明 <m15143310223@163.com>
2023-05-08 13:49:28 +0800

    4

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -9,8 +9,8 @@ export default hopeTheme({
   hostname: "https://mister-hope.github.io",
 
   author: {
-    name: "Mr.Hope",
-    url: "https://mrhope.site",
+    name: "田伟名",
+    url: "https://atianweiming.github.io/zh/",
   },
 
   iconAssets: "iconfont",
